fc974169a5550d1a37282be240741dd1ab614cfc
jet <jetvanommeren@gmail.com>
2024-01-22 14:48:33 +0100

    hill

--- code/algorithms/hill_climber.py
+++ code/algorithms/hill_climber.py
@@ -8,8 +8,6 @@ from code.classes.stations import Station
 from code.classes.trajectory import Trajectory
 from code.algorithms.baseline import Baseline
 
-from baseline import Baseline
-
 
 class Hillclimber:
     """ this class......."""
@@ -30,6 +28,68 @@ class Hillclimber:
         #set maximum of trajectories
         self.max_trajectories = max
 
+    def load_stations(self, filename):
+        """open file, read the lines and split into the three parts
+        """
+        with open(filename) as f:
+            # skip first line
+            next(f)
+            line = f.readline()
+            while line != "\n":
+                # remove newline character and split into different parts
+                parts = line.split(",", 2)
+
+                # set the different parts of the line to its variable
+                name = parts[0]
+                try:
+                    y = parts[1]
+                except(IndexError):
+                    break
+                x = parts[2].strip("\n")
+
+                # create the station, add station to self.stations
+                station = Station(name, y, x)
+                self.stations.append(station)
+
+                # read new line
+                line = f.readline()
+
+    def load_connections(self, filename):
+        """open file, read the lines and split into the three parts
+        """
+        with open(filename) as f:
+            # skip first line
+            next(f)
+            line = f.readline()
+            while line != "\n":
+                # remove newline character and split into different parts
+                parts = line.split(",", 2)
+
+                # set the different parts of the line to its variable
+                name = parts[0]
+                try:
+                    connection = parts[1]
+                except(IndexError):
+                    break
+                if "Holland" in filename:
+                    time = int(parts[2].strip("\n"))
+                else:
+                    time = int(parts[2].strip(".0\n"))
+
+                # add connection to connection list in Station class,
+                # and the list of all connections,
+                # for both stations
+                for station in self.stations:
+                    if name == station.name:
+                        station.add_connection(connection, time)
+                    if connection == station.name:
+                        station.add_connection(name, time)
+
+                # add the connection and time to the list of all connections
+                self.connections.append((name, connection))
+
+                # read new line
+                line = f.readline()
 
     def random_railmap(self):
         """Generate a random railmap using the Baseline class."""
@@ -47,8 +107,7 @@ class Hillclimber:
 
         # Calculate the quality of the generated railmap
         quality = self.baseline_instance.calculate_K()
-        print(quality)
         return quality
         
     def change_node(self):
-        pass
+        """Change a node/connection in the current trajectory."""
